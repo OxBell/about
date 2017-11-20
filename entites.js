754d3065c7ea9256030d058f5e72b6a0271398bb
Dylboy974 <mugidylan@gmail.com>
2017-11-20 09:44:53 +0400

    update db scheme

--- entites.js
+++ entites.js
@@ -1,10 +1,47 @@
 User {
 	id,
+	createdAt,
 	id_role,
 	name,
 	nickname,
 	photo
 }
+
+
+// Here’s what the same user would look like if they instead logged in with Facebook:
+
+/*{
+  "_id": "Ap85ac4r6Xe3paeAh",
+  "createdAt": "2015-12-10T22:29:46.854Z",
+  "services": {
+    "facebook": {
+      "accessToken": "XXX",
+      "expiresAt": 1454970581716,
+      "id": "XXX",
+      "email": "ada@lovelace.com",
+      "name": "Ada Lovelace",
+      "first_name": "Ada",
+      "last_name": "Lovelace",
+      "link": "https://www.facebook.com/app_scoped_user_id/XXX/",
+      "gender": "female",
+      "locale": "en_US",
+      "age_range": {
+        "min": 21
+      }
+    },
+    "resume": {
+      "loginTokens": [
+        {
+          "when": "2015-12-10T22:29:46.858Z",
+          "hashedToken": "XXX"
+        }
+      ]
+    }
+  },
+  "profile": {
+    "name": "Sashko Stubailo"
+  }
+}*/
 
 Role {
 	id,
@@ -13,39 +50,47 @@ Role {
 
 Poll {
 	id,
-	date(semaine),
-	choices : list of choice
+	date /*(semaine)*/,
+	choices : list of Choice,
+	state /*(current or finished)*/,
+	duration // (to add 24h if equal votes) 
 }
 
-// See how to link a user to a choice
+// See how to link a user to a choice to see who vote for what
 Choice {
 	id,
 	name,
 	datetime,
-	duree,
+	duration,
 	place,
-	nb_votes
+	user,
+	votes : list of Vote
+}
+
+Vote {
+	id,
+	user
 }
 
 Event {
 	id,
 	name,
 	datetime,
-	duree,
+	duration,
 	place,
-	users : list of users,
+	users : list of User,
 	forum : forum
 }
 
 Forum {
 	id,
-	topics : list of topic
+	topics : list of Topic
 }
 
 Topic {
 	id,
 	name,
-	posts : list of post
+	posts : list of Post
 }
 
 Post {
